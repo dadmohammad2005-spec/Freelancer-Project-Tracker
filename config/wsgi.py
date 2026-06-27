b89d352f6881a30e49fee0df8ae826ed4383ce46
Tumhara-GitHub-Username <dadmuhammad2005@gmail.com>
2026-06-27 14:30:58 +0500

    fix: vercel wsgi and config

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# application = get_wsgi_application()
+
+
+
+
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+app = application  # Vercel ke liye ZAROOR chahiye
